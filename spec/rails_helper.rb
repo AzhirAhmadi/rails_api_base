ddac0c8a2977ac0c4dd51bce000096f1cc446915
Azhir Ahmadi <aahmadi@dataguard.de>
2023-06-17 01:29:29 +0200

    clean up rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,9 +67,4 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
+
